e8f2c8c5965ef398fac0015267a7737f838f0fbf
Will Sackfield <will.sackfield@gmail.com>
2025-04-22 21:52:29 -0400

    Add new features to sports features

--- moneyball/__init__.py
+++ moneyball/__init__.py
@@ -7,4 +7,4 @@ try:
 except ImportError:
     pass
 
-__VERSION__ = "0.0.30"
+__VERSION__ = "0.0.31"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='moneyball',
-    version='0.0.30',
+    version='0.0.31',
     description='A library for determining what bets to make.',
     long_description=long_description,
     long_description_content_type='text/markdown',
