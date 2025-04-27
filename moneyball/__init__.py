f49af88cc3885f5d6e5d8dd7da2dc8eb2e4292f8
Will Sackfield <will.sackfield@gmail.com>
2025-04-26 21:53:00 -0400

    Update dependencies

--- moneyball/__init__.py
+++ moneyball/__init__.py
@@ -7,4 +7,4 @@ try:
 except ImportError:
     pass
 
-__VERSION__ = "0.0.38"
+__VERSION__ = "0.0.39"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='moneyball',
-    version='0.0.38',
+    version='0.0.39',
     description='A library for determining what bets to make.',
     long_description=long_description,
     long_description_content_type='text/markdown',
