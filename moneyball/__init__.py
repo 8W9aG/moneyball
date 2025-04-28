6a0d19643dae008c45dc13259171da1939f382e2
Will Sackfield <will.sackfield@gmail.com>
2025-04-28 18:29:03 -0400

    Bump version

--- moneyball/__init__.py
+++ moneyball/__init__.py
@@ -7,4 +7,4 @@ try:
 except ImportError:
     pass
 
-__VERSION__ = "0.0.48"
+__VERSION__ = "0.0.49"
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='moneyball',
-    version='0.0.48',
+    version='0.0.49',
     description='A library for determining what bets to make.',
     long_description=long_description,
     long_description_content_type='text/markdown',
