850b6972a12d71c0e18344dc69aa359623c52036
Will Sackfield <will.sackfield@gmail.com>
2025-09-14 19:00:13 -0400

    Update dependencies

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def install_requires() -> typing.List[str]:
 
 setup(
     name='moneyball',
-    version='0.0.222',
+    version='0.0.223',
     description='A library for determining what bets to make.',
     long_description=long_description,
     long_description_content_type='text/markdown',
